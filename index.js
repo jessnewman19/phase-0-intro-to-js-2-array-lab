1c97cc0a44754bcf81b5e23721016fe19e1da31a
Jessica Newman <jess.newman19@gmail.com>
2022-02-10 18:55:21 -0500

    commit tree 5c758509d31afaf8448814c513383a6926eda7da

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function destructivelyAppendCat (name) { 
+    return cats.push("Ralph");
+}
+
+destructivelyAppendCat();
+
+function destructivelyPrependCat (name) { 
+    return cats.unshift("Bob");
+}
+
+destructivelyPrependCat();
+
+function destructivelyRemoveLastCat () { 
+    return cats.splice(-1);
+}
+
+destructivelyRemoveLastCat();
+
+function destructivelyRemoveFirstCat () {
+    return cats.splice(0, 1);
+}
+
+destructivelyRemoveFirstCat();
+
+function appendCat (name) { 
+    const newCatNames = [...cats, "Broom"];
+    return newCatNames;
+}
+appendCat();
+
+function prependCat(name) {
+    const catNamesAgain = ["Arnold", ...cats]; 
+    return catNamesAgain;
+}
+
+prependCat();
+
+function removeLastCat() { 
+    const noLastCat = cats.slice(0, -1); 
+    return noLastCat;
+}
+
+removeLastCat();
+
+function removeFirstCat() { 
+    const noFirstCat = cats.slice(1);
+    return noFirstCat;
+}
+
+removeFirstCat();
